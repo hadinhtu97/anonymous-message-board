f07468a6625fcc2549548554b1547de0ac8e49cc
hadinhtu97 <53150506+hadinhtu97@users.noreply.github.com>
2021-01-29 03:38:53 +0700

    Initial commit

---
+++ routes/middleware.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const path = require('path');
+
+module.exports = (app) => {
+
+    app.use(express.static(path.join(__dirname, '../public')));
+
+    app.use(cors({ origin: '*' }));
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    app.use(helmet());
+    app.use(helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'"],
+            styleSrc: ["'self'"]
+        }
+    }));
+
+}
